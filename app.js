8e0d814ff7eb194602b8f1b17ed206e1ed232c0a
Pavithran S <pavithran.profession@gmail.com>
2025-03-20 08:22:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+function light_red() {
+    const redLight = document.querySelector('.red');
+    const yellowLight = document.querySelector('.yellow');
+    const greenLight = document.querySelector('.green');
+
+    redLight.style.backgroundColor = 'red';
+    yellowLight.style.backgroundColor = 'white';
+    greenLight.style.backgroundColor = 'white';
+}
+
+function light_yellow() {
+    const redLight = document.querySelector('.red');
+    const yellowLight = document.querySelector('.yellow');
+    const greenLight = document.querySelector('.green');
+
+    redLight.style.backgroundColor = 'white';
+    yellowLight.style.backgroundColor = 'yellow';
+    greenLight.style.backgroundColor = 'white';
+}
+
+function light_green() {
+    const redLight = document.querySelector('.red');
+    const yellowLight = document.querySelector('.yellow');
+    const greenLight = document.querySelector('.green');
+
+    redLight.style.backgroundColor = 'white';
+    yellowLight.style.backgroundColor = 'white';
+    greenLight.style.backgroundColor = 'green';
+}
